4e819fbd9fb0405240402dbf24ef9e959e1c8e69
Sparsh-R17 <sparsh170702@gmail.com>
2024-03-12 19:37:57 +0530

    Added APP UI

--- app/src/main/java/com/jetapp/quizapp/component/Questions.kt
+++ app/src/main/java/com/jetapp/quizapp/component/Questions.kt
@@ -1,20 +1,207 @@
 package com.jetapp.quizapp.component
 
-import android.util.Log
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jetapp.quizapp.model.QuestionItem
 import com.jetapp.quizapp.screens.QuestionsViewModel
+import com.jetapp.quizapp.util.AppColors
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
-        Log.d("LOAD", "Questions: Loading")
     } else {
-        questions?.forEach { questionItem ->
-            Log.d("RES", "Questions: ${questionItem.question}")
+        if (questions != null){
+            QuestionDisplay(question = questions.first())
         }
-        Log.d("SIZE", "Questions: ${questions?.size}")
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun QuestionDisplay(
+    question: QuestionItem,
+//    questionIndex: MutableState<Int>,
+//    viewModel: QuestionsViewModel,
+//    onNextClicked: (Int) -> Unit = {}
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+    }
+    val correctAnswerState = remember(question) {
+        mutableStateOf<Boolean?>(null)
+    }
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choicesState[it] == question.answer
+        }
+    }
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+//            .padding(4.dp),
+        color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+            DrawDottedLine(pathEffect = pathEffect)
+            Column {
+                Text(
+                    text = question.question,
+                    modifier = Modifier
+                        .fillMaxHeight(fraction = .3f)
+                        .padding(6.dp)
+                        .align(Alignment.Start),
+                    style = TextStyle(
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 22.sp,
+                        color = AppColors.moffWhite,
+                    )
+                )
+                //CHOICES
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .padding(3.dp)
+                            .border(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.moffDarkPurple,
+                                        AppColors.moffDarkPurple,
+                                    )
+                                ),
+                                width = 4.dp,
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50,
+                                )
+                            )
+                            .background(color = Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            modifier = Modifier
+                                .padding(start = 16.dp),
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = if (correctAnswerState.value == true && index == answerState.value) {
+                                    Color.Green.copy(alpha = .2f)
+                                } else {
+                                    Color.Red.copy(alpha = .2f)
+                                }
+                            ),
+                            selected = answerState.value == index,
+                            onClick = {
+                                updateAnswer(index)
+                            }
+                        )
+                        Text(text = answerText)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun QuestionTracker(
+    counter: Int = 10,
+    outOf: Int = 100,
+) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Question $counter/")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 18.sp
+                    )
+                ) {
+                    append(outOf.toString())
+                }
+            }
+        },
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
     }
 }
